944bd90568734e400de3464f5fced6474f256d94
Amartya Yadav <iamartyayadav@gmail.com>
2022-06-27 16:34:26 +0530

    reading and writing on Firestore

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -8,21 +9,43 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemBuilder: (context, index) => Container(
-          padding: EdgeInsets.all(8.0),
-          child: Text('This works!'),
-        ),
-        itemCount: 10,
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('chats/LifgOTwV6eojbeX9rVJj/messages')
+            .snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+          if (streamSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          List<Map<String, dynamic>> documents = [];
+          streamSnapshot.data!.docs.map((DocumentSnapshot document) {
+            Map<String, dynamic> data =
+                document.data()! as Map<String, dynamic>;
+            documents.add(data);
+          }).toList();
+          // print(documents);
+          // print();
+          return ListView.builder(
+            itemBuilder: (context, index) => Container(
+              padding: EdgeInsets.all(8.0),
+              child: Text(documents[index]['text']),
+            ),
+            itemCount: documents.length,
+          );
+        },
       ),
-      floatingActionButton: FloatingActionButton(onPressed: (){
-        FirebaseFirestore.instance.collection('chats/LifgOTwV6eojbeX9rVJj/messages').snapshots().listen((event) { 
-          // print(event.docs[0]['text']);
-          event.docs.forEach((element) {
-            print(element['text']);
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          CollectionReference msgs = FirebaseFirestore.instance.collection('chats/LifgOTwV6eojbeX9rVJj/messages');
+
+          msgs.add({
+            'text': 'added by plus button'
           });
-         });
-      },child: Icon(Icons.add),),
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
